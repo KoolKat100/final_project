d859383f2af7ca3a85a70a395a60de2129b7ae52
DhyutiGopal <dhyuti2732@gmail.com>
2018-07-20 12:09:00 -0400

    final project

---
+++ Dhyuti-DO-NOT-TOUCH!!!!/web-start/functions/index.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
+
+// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+
+// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+
+// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
+
+// TODO(DEVELOPER): Write the sendNotifications Function here.
+
+
+<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQMaw2DG8kqqxdXVrVgGgw5ZjuTKihap0",
+    authDomain: "friendlychat-e8043.firebaseapp.com",
+    databaseURL: "https://friendlychat-e8043.firebaseio.com",
+    projectId: "friendlychat-e8043",
+    storageBucket: "friendlychat-e8043.appspot.com",
+    messagingSenderId: "699290224390"
+  };
+  firebase.initializeApp(config);
+</script>
